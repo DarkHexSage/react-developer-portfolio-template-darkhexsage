9b20d57a18560e88cd5a26d3370852810e4ea5e2
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2026-01-02 17:30:01 -0600

    Update Details.js

--- src/Details.js
+++ src/Details.js
@@ -2,7 +2,6 @@
 // Logo images
 import logogradient from "./assets/logo.svg";
 import logo from "./assets/logo2.svg";
-import twitter from "./assets/twitter.svg";
 // Profile Image
 import profile from "./assets/profile.jpg";
 // Tech stack images
